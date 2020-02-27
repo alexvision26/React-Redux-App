156282547970e8fc59c9357bcb1b357d13f6cf0e
Alex Miller <alexmillerprojects@gmail.com>
2020-02-27 13:05:01 -0800

    fixed files

--- nba-app/src/App.js
+++ nba-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import PlayersList from './components/PlayersList';
